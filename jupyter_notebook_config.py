066a269d5d271df67a2707517f1d3e5e90431771
Mederic HURRIER <mederic.hurier@contractors.arcelormittal.com>
2020-02-26 16:04:01 +0100

    disable security

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,4 +1,8 @@
 # Configuration file for jupyter-notebook.
 
+c.NotebookApp.token = ''
+c.NotebookApp.password = ''
 c.NotebookApp.open_browser = False
+c.NotebookApp.disable_check_xsrf = True
+
 c.ContentsManager.allow_hidden = True
