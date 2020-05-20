0d4526ed7f35f9f474eaabc4e2df1299d50a0adb
Médéric Hurier <fmind@fmind.me>
2020-05-20 20:47:59 +0200

    switch to 2.0

--- extensions.py
+++ extensions.py
@@ -1,21 +1,53 @@
 #!/usr/bin/env python3
 
 import os
+import argparse
+
+PACKAGES = [
+    'voila',
+    'nbdime',
+    'jupytext',
+    'jupyter-lsp',
+    'jupyterlab-git',
+    'jupyterlab-quickopen',
+    'python-language-server[all]',
+]
 
 EXTENSIONS = [
     '@jupyterlab/toc',
-    'nbdime-jupyterlab',
     'jupyterlab-jupytext',
+    '@krassowski/jupyterlab-lsp',
+    '@parente/jupyterlab-quickopen',
+    '@jupyter-voila/jupyterlab-preview',
+    '@jupyter-widgets/jupyterlab-manager',
 ]
 
+parser = argparse.ArgumentParser('extensions.py')
+parser.add_argument('--user', action='store_true')
+
+
+def build_extension() -> int:
+    return os.system('jupyter lab build')
+
+
+def install_extension(extension: str) -> int:
+    return os.system(f'jupyter labextension install {extension} --no-build')
+
+
+def install_package(package: str, user: bool = False) -> int:
+    return os.system(f'python3 -m pip install {"--user" if user else ""} "{package}"')
+
+
 if __name__ == '__main__':
+    args = parser.parse_args()
+
+    for package in PACKAGES:
+        print(f'Installing package: {package} {args.user}')
+        print(f'-> {install_package(package, args.user)}')
+
     for extension in EXTENSIONS:
-        print(
-            f'Installed {extension} ->',
-            os.system(f'jupyter labextension install --no-build "{extension}"'),
-        )
-        
-    print(
-        'Extensions built ->',
-        os.system('jupyter lab build')
-     )
+        print(f'Installing extension: {extension}')
+        print(f'-> {install_extension(extension)}')
+
+    print(f'Building extension: {len(EXTENSIONS)}')
+    print(f'-> {build_extension()}\n\nBuild: DONE')
